f8fd9acde88670f610621f10daf2a141de5de679
Seibi <seibi@Seibis-MacBook-Air.local>
2022-11-06 01:57:05 -0500

    ECG noise

--- KobaraNoiseAlteration.m
+++ KobaraNoiseAlteration.m
@@ -1,25 +1,7 @@
-%
-% Test program for colored noise generation.
-%
-% Dependencies: The baseline wander toolbox of the Open Source ECG Toolbox
-%
-% Open Source ECG Toolbox, version 1.0, November 2006
-% Released under the GNU General Public License
-% Copyright (C) 2006  Reza Sameni
-% Sharif University of Technology, Tehran, Iran -- LIS-INPG, Grenoble, France
-% reza.sameni@gmail.com
+% Noise simulation on ECG.
+% Codes are obtained from 'testNoiseGenerator.m'.
+% SK changed the weight of muscle artifact noise to 5.
 
-% This program is free software; you can redistribute it and/or modify it
-% under the terms of the GNU General Public License as published by the
-% Free Software Foundation; either version 2 of the License, or (at your
-% option) any later version.
-% This program is distributed in the hope that it will be useful, but
-% WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-% Public License for more details. You should have received a copy of the
-% GNU General Public License along with this program; if not, write to the
-% Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-% MA  02110-1301, USA.
 
 clc
 clear
@@ -48,7 +30,7 @@ noisetype = 5;
 beta = 1.5;     % noise color (for noisetype = 1)
 w_bw = 1;       % weight of baseline wander noise in the generated noise (for noisetype = 5)
 w_em = 1;       % weight of electrode movement noise in the generated noise (for noisetype = 5)
-w_ma = 1;       % weight of muscle artifact noise in the generated noise (for noisetype = 5)
+w_ma = 5;       % weight of muscle artifact noise in the generated noise (for noisetype = 5)
 
 switch noisetype
     case 0     % white noise
